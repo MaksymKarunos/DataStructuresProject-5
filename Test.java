160fcf84094991729476fe2a5ddcf53dd36c8633
Maksym <maksim.karunos@gmail.com>
2019-11-15 13:18:57 -0500

    commiting initial files

---
+++ Test.java
@@ -0,0 +1,8 @@
+import java.java.util.*;
+
+public class Test{
+    public static void main(String[] args){
+        HashMap<Integer,String> myMap = new HashMap<Integer,String> (1000,0.75);
+
+    }
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 =======================
 # DATA STRUCTURES PROJECT NUMBER 5
 =======================
-sfsdfsdf
+
