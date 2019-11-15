24d7528ba8bfa7d7c4d92cd3fb694c1dcc457949
MaksymKarunos <maksim.karunos@gmail.com>
2019-11-15 18:54:49 -0500

    TO-DO Dates updated

--- Test.java
+++ Test.java
@@ -2,14 +2,31 @@ import java.util.*;
 
 public class Test{
     public static void main(String[] args){
-        Hashtable <String, Integer> myHashTable = new Hashtable<>(1000, 0.75);
-        BinarySearchTree myAVL = new BinarySearchTree<>();
+        // INSTANCE OF THE OBJECTS
+        // 0.75 IS DEFAULT LOAD FACTOR
+        Hashtable <String, Integer> myHashTable = new Hashtable<String, Integer>(1000);
+        BinarySearchTree<String> myAVL = new BinarySearchTree<>();
+
         
+        /* TO DO FOR PAULA 
+        COMPLETE PARTS 1 AND 2
+        COMPLETE THE HEADER OF THIS CLASS: NAME, PROJECT, OUTPUT, INPUT ETC.
+         //DEADLINE FOR YOU:
+        //=======================
+        // SAT, NOV 16, 23:59:59=
+        //======================
+
+
         //TO-DO ALEJANDRO GONZALEZ
         // CREATE A SCANNER THAT IS GOING TO READ int N 
-        long startTime = System.nanoTime();
-        // CREATE A LOOP TILL N, AND PUT KEYS AND VALUES FOR INSERT
-        //
-        //myHashTable.put(key, value)
+        // MEASURE TIME AND COMPLETE THE TABLE ABOVE
+        // WHEN YOU START A CALL USE 
+            //long startTime = System.nanoTime();
+        // WHEN YOU FINISH A CALL USE 
+            //long ENDTime = System.nanoTime();
+        // USE OUTPUT TO OUTPUT END-START;
+        // FOLLOW THE ASSIGNMENT
+        // REACH ME OUT FOR QUESTIONS
+        */
     }
 }
