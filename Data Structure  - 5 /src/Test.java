def5b4c5aadfa3349712ad744c45faa1f08d3333
Paul Hernandez <paulhernandez@Pauls-MacBook-Air.local>
2019-11-19 12:30:38 -0500

    last trial

--- Data Structure  - 5 /src/Test.java
+++ Data Structure  - 5 /src/Test.java
@@ -63,11 +63,7 @@ public class Test {
         for (int i = 0; i < mArrayMapping.getSize(); i++) {
             mArrayMapping.insert(i);
         }
-        ///blblallbllsdfsldf'sd
-        /// coding , eating , sleeping
-        // PAULLLLLLLLAAAAA
-        ///blblallbllsdfsldf'sd
-        /// coding , eating , sleeping
+        
         
         myScanner.close();
     }
